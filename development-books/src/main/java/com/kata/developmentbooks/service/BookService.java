a7a8c737282810549849a0bb49737f9c322910ee
2022-DEV3-121 <2022dev2121@gmail.com>
2022-12-19 00:30:06 +0530

    refactor: extracted business logic into service class.

---
+++ development-books/src/main/java/com/kata/developmentbooks/service/BookService.java
@@ -0,0 +1,15 @@
+package com.kata.developmentbooks.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+	public List<String> getAllBooks() {
+		return Arrays.asList("Clean Code", "The Clean Coder", "Clean Architecture",
+				"Test Driven Development by Example", "Working Effectively With Legacy Code");
+	}
+}
