5a97135aee7059eb9848075b86d1475d6ce10fa0
2022-DEV3-121 <2022dev2121@gmail.com>
2022-12-19 12:05:30 +0530

    feat: get all books should return all book with details.

--- development-books/src/test/java/com/kata/developmentbooks/controller/DevelopmentBooksControllerTest.java
+++ development-books/src/test/java/com/kata/developmentbooks/controller/DevelopmentBooksControllerTest.java
@@ -1,14 +1,13 @@
 package com.kata.developmentbooks.controller;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.kata.developmentbooks.model.Books;
 import com.kata.developmentbooks.service.BookService;
 
 class DevelopmentBooksControllerTest {
@@ -23,20 +22,14 @@ class DevelopmentBooksControllerTest {
 
 	@Test
 	void getBookNameShouldReturnNameOfTheBook() {
-		List<String> result = controller.getAllBooks();
+		List<Books> result = controller.getAllBooks();
 		assertEquals("Clean Code", result.get(0));
 	}
 
 	@Test
 	public void getAllBooksShouldReturnFiveBookNames() {
-		List<String> books = controller.getAllBooks();
+		List<Books> books = controller.getAllBooks();
 		assertEquals(5, books.size());
-		assertTrue(books.containsAll(getAllBooks()));
-	}
-
-	private List<String> getAllBooks() {
-		return Arrays.asList("Clean Code", "The Clean Coder", "Clean Architecture",
-				"Test Driven Development by Example", "Working Effectively With Legacy Code");
 	}
 
 }
--- development-books/src/test/java/com/kata/developmentbooks/service/BookServiceTest.java
+++ development-books/src/test/java/com/kata/developmentbooks/service/BookServiceTest.java
@@ -3,24 +3,43 @@ package com.kata.developmentbooks.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import com.kata.developmentbooks.model.Books;
 
 class BookServiceTest {
 
 	@Test
 	public void getAllBooksShouldReturnFiveBookNames() {
 		BookService service = new BookService();
-		List<String> books = service.getAllBooks();
+		List<Books> books = service.getAllBooks();
 		assertEquals(5, books.size());
-		assertTrue(books.containsAll(getAllBooks()));
+		assertTrue(books.containsAll(getExpectedBooks()));
 	}
 
-	private List<String> getAllBooks() {
-		return Arrays.asList("Clean Code", "The Clean Coder", "Clean Architecture",
-				"Test Driven Development by Example", "Working Effectively With Legacy Code");
+	@Test
+	public void getAllBooksShouldReturnBooksWithDetails() {
+		BookService service = new BookService();
+		List<Books> result = service.getAllBooks();
+		List<Books> excpectedBooks = getExpectedBooks();
+		assertEquals(excpectedBooks.get(0), result.get(0));
+		assertEquals(excpectedBooks.get(1), result.get(1));
+		assertEquals(excpectedBooks.get(2), result.get(2));
+		assertEquals(excpectedBooks.get(3), result.get(3));
+		assertEquals(excpectedBooks.get(4), result.get(4));
+	}
+
+	private List<Books> getExpectedBooks() {
+		List<Books> excpectedBooks = new ArrayList<Books>();
+		excpectedBooks.add(new Books(1, "Clean Code", "Robert Martin", 2008, 50.00));
+		excpectedBooks.add(new Books(2, "The Clean Coder", "Robert Martin", 2011, 50.00));
+		excpectedBooks.add(new Books(3, "Clean Architecture", "Robert Martin", 2017, 50.00));
+		excpectedBooks.add(new Books(4, "Test-Driven Development By Example", "Kent Beck", 2003, 50.00));
+		excpectedBooks.add(new Books(5, "Working Effectively With Legacy Code", "Michael C. Feathers", 2004, 50.00));
+		return excpectedBooks;
 	}
 
 }
